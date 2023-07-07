9c5df4d2620a334710e713741c4579c0ab7b46eb
Pritam96 <pritammondal96@gmail.com>
2023-07-07 12:33:20 +0530

    Implementing Blowfish encryption to encrypt the password

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const ErrorResponse = require('../utils/errorResponse');
+const bcrypt = require('bcrypt');
 
 // @desc Create new user
 // @route POST /user/add
@@ -9,17 +10,21 @@ exports.postCreateUser = async (req, res, next) => {
   const password = req.body.password;
 
   try {
-    const user = await User.create({
-      name: name,
-      email: email,
-      password: password,
-    });
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, async (err, hashData) => {
+      if (err) console.log(err);
+      const user = await User.create({
+        name: name,
+        email: email,
+        password: hashData,
+      });
 
-    console.log('user created');
+      console.log('user created');
 
-    res.status(201).json({
-      success: true,
-      data: user,
+      res.status(201).json({
+        success: true,
+        data: user,
+      });
     });
   } catch (error) {
     next(
@@ -46,17 +51,12 @@ exports.postLoginUser = async (req, res, next) => {
       throw 'User with this email id not exist';
     }
 
-    const user_password = await User.findAll({
-      where: { password: password },
-    });
-
-    if (!user_password[0]) {
-      throw 'Wrong email id or password';
-    }
-
-    console.log('User Logged in');
-    res.status(201).json({
-      success: true,
+    bcrypt.compare(password, user_email[0].password, (err, data) => {
+      if (err) throw 'Wrong email id or password';
+      console.log('User Logged in');
+      res.status(201).json({
+        success: true,
+      });
     });
   } catch (error) {
     next(new ErrorResponse(error, 409));
